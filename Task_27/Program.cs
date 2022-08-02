8443c81c00cd956ba18b5e614a535dae14550715
Svet2018 <bulatova44@gmail.com>
2022-08-02 22:33:54 +0300

    The final solution of this task

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -2,3 +2,31 @@
 //452 -> 11
 //82 -> 10
 //9012 -> 12
+
+
+int GetNum()
+{
+    System.Console.WriteLine("Ввведите число:");
+    int argument = Convert.ToInt32(Console.ReadLine());
+    return argument;
+}
+
+int SumNum(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+void PrintToConsole(int arg)
+{
+    System.Console.WriteLine($"Сумма цифр, введенного Вами числа равна - {arg}");
+}
+
+
+PrintToConsole(SumNum(GetNum()));
+
