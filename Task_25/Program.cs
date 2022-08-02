9f153e5d1305123b718ee0aa7ff80a6ba71874ef
Svet2018 <bulatova44@gmail.com>
2022-08-02 23:15:52 +0300

    The final solution of this task

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -3,3 +3,28 @@
 //2, 4 -> 16
 
 
+int GetNum()
+{
+    System.Console.WriteLine("Ввведите число:");
+    int argument = Convert.ToInt32(Console.ReadLine());
+    return argument;
+}
+
+int PowNum(int num1, int num2)
+{
+    int resalt = 1;
+    while (num2 > 0)
+    {
+        resalt = resalt * num1;
+        num2--;
+    }
+    return resalt;
+}
+
+void PritnResalt(int resalt)
+{
+    System.Console.WriteLine($"Результат возведения в натуральную степень - {resalt}");
+}
+
+PritnResalt(PowNum(GetNum(), GetNum()));
+
